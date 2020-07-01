3f026cb2228530aa9dea5bf6de03cc931feb8c63
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-07-01 13:13:44 +0300

    part 1 - added useRouteMatch() and useParams() hooks

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Modal from "./Modal";
 
 function App() {
     const history = useHistory();
+    const colors = ['purple', 'green', 'red'];
 
     return (
         <div>
-            <Link to="/purple">
+            <Link to={`/${colors[0]}`}>
                 <div className="square purple-square"/>
             </Link>
-            <Link to="/purple" className="link">Purple</Link>
-            <Link to="/green">
+            <Link to={`/${colors[0]}`} className="link">Purple</Link>
+            <Link to={`/${colors[1]}`}>
                 <div className="square green-square"/>
             </Link>
-            <Link to="/green" className="link">Green</Link>
-            <Link to="/red">
+            <Link to={`/${colors[1]}`} className="link">Green</Link>
+            <Link to={`/${colors[2]}`}>
                 <div className="square red-square"/>
             </Link>
-            <Link to="/red" className="link">Red</Link>
+            <Link to={`/${colors[2]}`} className="link">Red</Link>
             <Switch>
-                <Route path="/purple">
-                   <Modal colorName="purple" onClose={() => history.push("/")}/>
+                <Route path="/:colorName">
+                   <Modal onClose={() => history.push("/")}/>
                 </Route>
-                <Route path="/green">
-                    <Modal colorName="green" onClose={() => history.push("/")}/>
+                <Route path="/:colorName">
+                    <Modal onClose={() => history.push("/")}/>
                 </Route>
-                <Route path="/red">
-                    <Modal colorName="red" onClose={() => history.push("/")}/>
+                <Route path="/:colorName">
+                    <Modal onClose={() => history.push("/")}/>
                 </Route>
             </Switch>
         </div>
