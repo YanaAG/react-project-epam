f243b09f7f1ef4791a076f41d91e28991b11eacd
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-07-11 18:09:40 +0300

    part 2 - create additional action creator with a type: "SQUARE_COUNT"

--- src/action-creator.js
+++ src/action-creator.js
@@ -7,12 +7,22 @@ function changeCount(amount = 1) {
     }
 }
 
+function squareCount() {
+    return {
+        type: "SQUARE_COUNT"
+    }
+}
+
 function reducer(state = {count: 0}, action) {
     // return new state based on the incoming action.type
     switch(action.type) {
         case "CHANGE_COUNT":
             return {
                 count: state.count + action.payload
+            };
+        case "SQUARE_COUNT":
+            return {
+                count: state.count ** 2
             };
         default:
             return state
@@ -22,9 +32,14 @@ function reducer(state = {count: 0}, action) {
 const store = redux.createStore(reducer);
 
 store.subscribe(() => {
-    console.log('action creator', store.getState())
+    const { count } = store.getState();
+    console.log('action creator', count);
 });
 
 store.dispatch(changeCount(5));
 store.dispatch(changeCount(5));
 store.dispatch(changeCount(-5));
+
+store.dispatch(squareCount());
+store.dispatch(squareCount());
+store.dispatch(squareCount());
